4f7f4895f1a5a1f50c00673ef6ee00f75578850c
sumitgirigit <sgiri5667@gmail.coom>
2021-08-10 13:49:30 +0530

    Nav Bar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import React from 'react';
  import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+ import NavBar from './components/NavBar';
+
 function App() {
   return (
     <React.Fragment>
 
-      <h1>Portfolio Project.</h1>
-      <button className="btn btn-success"> Click Me</button>
+      <NavBar />
 
     </React.Fragment>
   );
