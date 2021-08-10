7e8b808a89a3206e7eff6790b35b38c71217e6e8
sumitgirigit <sgiri5667@gmail.com>
2021-08-10 13:03:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <React.Fragment>
+
+      <h1>Portfolio Project</h1>
+      <button className="btn btn-success"> Click Me</button>
+
+    </React.Fragment>
+  );
+}
+
+export default App;
